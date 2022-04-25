7f46c5eb860209a48b678c5c31eb5c3f2206eb59
leehyoseop <64147276+leehyoseop@users.noreply.github.com>
2022-04-25 15:45:05 +0900

    Add files via upload

---
+++ ojt/src/main/java/com/example/demo/employee/entity/EmployeeEntity.java
@@ -0,0 +1,12 @@
+package com.example.demo.employee.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+//Test
+public class EmployeeEntity {
+	private String id;
+	private String name;
+}
